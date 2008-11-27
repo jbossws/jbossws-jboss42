3bdfcc8ae99dec90f31af741ea06b82542847197
richard.opalka@jboss.com <richard.opalka@jboss.com>
2008-11-27 15:37:58 +0000

    [JBWS-2268] fix package

--- src/main/java/org/jboss/wsf/container/jboss42/InvocationHandlerJSE.java
+++ src/main/java/org/jboss/wsf/container/jboss42/InvocationHandlerJSE.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.wsf.container.jboss50;
+package org.jboss.wsf.container.jboss42;
 
 import java.lang.reflect.Method;
 
